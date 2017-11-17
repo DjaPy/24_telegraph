44b8b914602e4c7265a0769139922f254f313868
Ivanov Sergey <djapy@yandex.ru>
2017-11-17 16:13:37 +0300

    Adds the indexicality of the table and correct primary key

--- project/models.py
+++ project/models.py
@@ -3,15 +3,14 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class Story(db.Model):
 
-    slug = db.Column(db.String(32), primary_key=True)
+class Story(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    author_id = db.Column(db.String(64))
+    slug = db.Column(db.String(32), index=True)
     story_title = db.Column(db.String(128))
     story_signature = db.Column(db.String(64))
     story_body = db.Column(db.Text)
-    author_id = db.Column(db.String(64))
-    story_uuid = db.Column(db.String(128))
-
 
     def __repr__(self):
         return '<Story {}>'.format(self.story_title)

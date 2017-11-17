6bf1df72a16515222be94b60c4d008ec77b32c7f
Ivanov Sergey <djapy@yandex.ru>
2017-11-17 16:11:49 +0300

    Fixes issues related to incorrect display

--- project/views.py
+++ project/views.py
@@ -1,15 +1,17 @@
 
-from flask import render_template, request, redirect, url_for, make_response
+from flask import render_template, request, redirect, url_for, make_response, abort
 import uuid
+from re import sub
 from project import app, db
 from .models import Story
+
 
 HOURS_LIVE_COOKIES = 24
 MAX_AGE_LIVE_COOKIES = HOURS_LIVE_COOKIES * 60 * 60
 
 
 def get_slug(item_form_title):
-    slug = item_form_title.replace(' ', '_')
+    slug = sub('[\s/]', '_', item_form_title)
     return slug
 
 
@@ -26,17 +28,15 @@ def add_story():
         author_id = request.cookies.get('cookie')
         if not author_id:
             author_id = str(uuid.uuid4())
-        story_uuid = str(uuid.uuid4())
         story_title = request.form.get('header')
         story_signature = request.form.get('signature')
         story_body = request.form.get('body')
         slug = get_slug(story_title)
-        db.session.add(Story(slug=slug,
+        db.session.add(Story(author_id=author_id,
+                             slug=slug,
                              story_title=story_title,
                              story_signature=story_signature,
-                             story_body=story_body,
-                             story_uuid=story_uuid,
-                             author_id=author_id))
+                             story_body=story_body))
         db.session.commit()
         new_story = make_response(redirect(url_for('view_text',
                                                        slug=slug)))
@@ -50,16 +50,14 @@ def add_story():
 @app.route('/<slug>')
 def view_text(slug):
     author_id = request.cookies.get('author_id')
-    story = db.session.query(Story).get_or_404(slug)
-    return render_template('story.html',
-                           story=story,
-                           author_id=author_id)
+    story = db.session.query(Story).filter(Story.slug == slug).first_or_404()
+    return render_template('story.html',story=story, author_id=author_id)
 
 
 @app.route('/edit/<slug>', methods=['GET', 'POST'])
 def edit_text(slug):
     author_id = request.cookies.get('author_id')
-    story = db.session.query(Story).get_or_404(slug)
+    story = db.session.query(Story).filter(Story.slug == slug).first()
     if author_id == story.author_id:
         if request.method == 'POST':
             story_title = request.form.get('header')
@@ -72,4 +70,14 @@ def edit_text(slug):
             update_text(edit_text)
         return render_template('form.html', story=story)
     else:
-        return "Access is denied! Create your own story!"
+        abort(403)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(403)
+def page_not_found(e):
+    return render_template('403.html'), 403
